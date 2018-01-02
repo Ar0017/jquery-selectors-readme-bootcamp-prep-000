6725aeed2f55c048f55a217563244cb62cafdca2
Arinze <arinzedile@yahoo.com>
2018-01-02 23:34:57 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,9 +1,8 @@
 // declare your functions here...
 function paragraphSelector() {
-  $('p')
-  return 'p'
+return  $('p')
 }
 
 function lastImageSelector() {
-  $('div img:last-child')
+  return $('div img:last-child')
 }
